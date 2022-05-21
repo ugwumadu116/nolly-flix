347b564778b2c4d2f07d5b07d0baf1c7be8c4524
ugwumadu116 <ugwumadu116@gmail.com>
2022-05-21 21:32:11 +0100

    added movies and handle like

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from 'react';
 
 import './App.css';
 import styled from "styled-components";
+import { AiFillHeart } from "react-icons/ai";
 
 
 
 
 function App() {
 
+  const [dbMonito, setDbMonito] = useState(false)
   const [movies, setMovies] = useState([
     {
       id:1,
@@ -41,18 +43,37 @@ function App() {
       id:5,
       name:"Simply Javascript ",
       img:"https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1388830159l/1380411.jpg",
-      like:1,
+      like:5,
       price:100
     },
     {
       id:6,
       name:"The New Web ",
       img:"https://mytechdecisions.com/wp-content/uploads/2019/10/AdobeStock_256229414.jpg",
-      like:1,
+      like:10,
       price:100
     },
    
   ]);
+
+  useEffect(() => {
+    console.log("changed");
+  }, [dbMonito])
+
+  const handleLikes = (item) => {
+    let newMovies = movies
+
+    let selectedMovieIndex =  movies.findIndex(val => val.id === item.id)
+    newMovies[selectedMovieIndex] = {
+      ...item,
+      like: item.like+1,
+      price: item.price+10,
+    }
+  
+    setMovies(newMovies)
+    setDbMonito(!dbMonito)
+
+  }
 
 
     return (
@@ -89,6 +110,56 @@ Lorem Ipsum is simply dummy text of the printing and typesetting industry. <br /
 
     </MoviesHero>
 
+    <h2 className='releases'>New releases</h2>
+    <div className='movie-card-wrapper'>
+      {
+        movies.sort((a, b) => b.like - a.like ).map((item, i) => (
+<MovieCard key={i} bg={item.img}>
+    <div className='movieoverlay'>
+      </div>
+      <div className='movie-info'>
+      <div className='cat'>
+          <span>Fantasy</span>
+        </div>
+
+        <svg width="80" height="16" viewBox="0 0 80 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M8.00001 3.61352L8.95841 6.36519L9.13371 6.8685H9.66668H12.5206L10.3106 8.62805L9.91741 8.94114L10.0575 9.42385L10.8685 12.2179L8.41985 10.5637L8.00001 10.2801L7.58017 10.5637L5.13155 12.2179L5.94251 9.42385L6.08261 8.94114L5.68939 8.62805L3.47942 6.8685H6.33334H6.86631L7.04161 6.36519L8.00001 3.61352Z" fill="white" stroke="white" stroke-width="1.5"/>
+        <path d="M24 3.61352L24.9584 6.36519L25.1337 6.8685H25.6667H28.5206L26.3106 8.62805L25.9174 8.94114L26.0575 9.42385L26.8685 12.2179L24.4199 10.5637L24 10.2801L23.5802 10.5637L21.1316 12.2179L21.9425 9.42385L22.0826 8.94114L21.6894 8.62805L19.4794 6.8685H22.3333H22.8663L23.0416 6.36519L24 3.61352Z" fill="white" stroke="white" stroke-width="1.5"/>
+        <path d="M40 3.61352L40.9584 6.36519L41.1337 6.8685H41.6667H44.5206L42.3106 8.62805L41.9174 8.94114L42.0575 9.42385L42.8685 12.2179L40.4199 10.5637L40 10.2801L39.5802 10.5637L37.1316 12.2179L37.9425 9.42385L38.0826 8.94114L37.6894 8.62805L35.4794 6.8685H38.3333H38.8663L39.0416 6.36519L40 3.61352Z" fill="white" stroke="white" stroke-width="1.5"/>
+        <path d="M56 3.61352L56.9584 6.36519L57.1337 6.8685H57.6667H60.5206L58.3106 8.62805L57.9174 8.94114L58.0575 9.42385L58.8685 12.2179L56.4199 10.5637L56 10.2801L55.5802 10.5637L53.1316 12.2179L53.9425 9.42385L54.0826 8.94114L53.6894 8.62805L51.4794 6.8685H54.3333H54.8663L55.0416 6.36519L56 3.61352Z" fill="white" stroke="white" stroke-width="1.5"/>
+        <path d="M72 3.61352L72.9584 6.36519L73.1337 6.8685H73.6667H76.5206L74.3106 8.62805L73.9174 8.94114L74.0575 9.42385L74.8685 12.2179L72.4199 10.5637L72 10.2801L71.5802 10.5637L69.1316 12.2179L69.9425 9.42385L70.0826 8.94114L69.6894 8.62805L67.4794 6.8685H70.3333H70.8663L71.0416 6.36519L72 3.61352Z" fill="white" stroke="white" stroke-width="1.5"/>
+        </svg>
+
+        <h4 className='mv-name'>{item.name}</h4>
+
+        <div className='d-flex'>
+        <span className='fantasy-price'> ₦{item.price}</span>
+
+      
+
+
+        </div>
+        <button onClick={() => handleLikes(item)} className='hero-watch'>
+        <AiFillHeart size={15}  color='red' /> {item.like} Likes 
+        </button>
+
+
+      </div>
+
+    </MovieCard>
+        ))
+      }
+    
+
+
+
+
+    </div>
+    
+
+
+   
+
 
   
 
@@ -97,6 +168,115 @@ Lorem Ipsum is simply dummy text of the printing and typesetting industry. <br /
   );
 }
 
+
+const MovieCard = styled.div`
+width: 292px;
+height: 440px;
+margin: 10px;
+background-image: url(${({ bg }) => ( bg)}) ;
+position: relative;
+background-position: center;
+         background-repeat: no-repeat;
+         background-size: cover;
+.movieoverlay{
+  width: 100% ;
+  background: linear-gradient(180deg, rgba(29, 29, 29, 0) 0%, rgba(29, 29, 29, 0.8) 80.79%);
+height:100%; 
+position: absolute;
+
+}
+
+
+.movie-like{
+  /* margin:0px auto; */
+  cursor:pointer;
+  margin-top:20px;
+  width: 90%;
+height: 26px;
+background: #BA104D;
+box-shadow: 0px 16px 16px 4px rgba(0, 0, 0, 0.12);
+border-radius: 40px;
+border:none;
+/* background: transparent ; */
+
+font-weight: 500;
+font-size: 18px;
+line-height: 24px;
+
+text-align: center;
+color: #FFFFFF;
+
+text-shadow: 0px 40px 40px rgba(0, 0, 0, 0.12);
+
+background: #1D1D1D;
+/* Primary/Razzmatazz */
+
+border: 1px solid #E10856;
+box-shadow: 0px 16px 16px 4px rgba(0, 0, 0, 0.12);
+border-radius: 10px;
+}
+
+.sds--e{
+margin-bottom: 16px;
+
+}
+.mv-name{
+  font-weight: 500;
+font-size: 24px;
+line-height: 32px;
+margin:16px 0px ;
+
+color: #FFFFFF;
+}
+.cat{
+  background: rgba(29, 29, 29, 0.5);
+border-radius: 0px 8px;
+padding:4px 8px;
+display:flex;
+justify-content: center;
+align-items:center;
+margin-bottom:16px;
+span{
+  font-weight: 400;
+font-size: 14px;
+line-height: 16px;
+
+color: #0FEFFD;
+}
+}
+
+.hero-watch{
+  width: 150px;
+height: 36px;
+border: 1px solid #E10856;
+box-shadow: 0px 16px 16px 4px rgba(0, 0, 0, 0.12);
+border-radius: 40px;
+background: transparent ;
+
+font-weight: 500;
+font-size: 18px;
+line-height: 24px;
+
+text-align: center;
+color: #FFFFFF;
+
+text-shadow: 0px 40px 40px rgba(0, 0, 0, 0.12);
+margin-bottom: 20px;
+}
+
+.movie-info{
+  height:100% ;
+  padding-left:5px;
+  width:100% ;
+  display:flex;
+  position: absolute;
+  align-items:flex-start;
+  justify-content: flex-end;
+  flex-direction:column;
+
+
+}
+`;
 const MoviesHero = styled.div`
 background-image: url("https://m.media-amazon.com/images/M/MV5BZWVjMmY4ODUtMmYzMC00NDdjLWE4MjMtOWUzMDBlNzczOGMwXkEyXkFqcGdeQWpnYW1i._V1_.jpg");
 background-position: center;
